b91c25e19cd548b6a170642c37e2c7119ef003c0
James Townsend <j.a.townsend2000@gmail.com>
2021-08-05 11:03:52 +0100

    Do loop tests

--- test/loops/test_loops.py
+++ test/loops/test_loops.py
@@ -44,3 +44,40 @@ def test_loop_while_infinite():
     assert len(lines) == 2
     assert lines[0] == "1000"
     assert lines[1] == ""
+
+def test_loop_do_basic_loop():
+    completed = subprocess.run(["bin/canidae",  "test/loops/do/basic_loop.can"], text=True, capture_output=True)
+    assert completed.returncode == 0
+    lines = completed.stdout.split("\n")
+    assert len(lines) == 2
+    assert lines[0] == "11"
+    assert lines[1] == ""
+
+def test_loop_do_break():
+    completed = subprocess.run(["bin/canidae",  "test/loops/do/break.can"], text=True, capture_output=True)
+    assert completed.returncode == 0
+    lines = completed.stdout.split("\n")
+    assert len(lines) == 3
+    assert lines[0] == "3"
+    assert lines[1] == "10"
+    assert lines[2] == ""
+
+def test_loop_do_continue():
+    completed = subprocess.run(["bin/canidae",  "test/loops/do/continue.can"], text=True, capture_output=True)
+    assert completed.returncode == 0
+    lines = completed.stdout.split("\n")
+    assert len(lines) == 6
+    assert lines[0] == "1"
+    assert lines[1] == "2"
+    assert lines[2] == "4"
+    assert lines[3] == "5"
+    assert lines[4] == "10"
+    assert lines[5] == ""
+
+def test_loop_do_infinite():
+    completed = subprocess.run(["bin/canidae",  "test/loops/do/infinite.can"], text=True, capture_output=True)
+    assert completed.returncode == 0
+    lines = completed.stdout.split("\n")
+    assert len(lines) == 2
+    assert lines[0] == "1000"
+    assert lines[1] == ""
