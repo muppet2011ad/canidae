05e3774ad380d6c6ace9ea0996ccbf6308b11bcf
James Townsend <j.a.townsend2000@gmail.com>
2021-08-24 22:59:44 +0100

    Fix typo causing string interning bug

--- src/hashmap.c
+++ src/hashmap.c
@@ -128,7 +128,7 @@ void mark_hashmap(VM *vm, hashmap *h) {
 void hashmap_remove_white(VM *vm, hashmap *h) {
     for (uint32_t i = 0; i < h->capacity; i++) {
         kv_pair *entry = &h->entries[i];
-        if (entry->k != NULL && entry->k->obj.is_marked) {
+        if (entry->k != NULL && !entry->k->obj.is_marked) {
             hashmap_delete(h, entry->k);
         }
     }
